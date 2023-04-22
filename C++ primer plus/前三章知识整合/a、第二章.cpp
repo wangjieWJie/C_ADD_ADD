1d9531db80a4ec32ce0aea609795a9f539dbfd94
pamapama88 <3512232084@qq.com>
2023-04-22 13:40:06 +0800

    Go ahead！！！

---
+++ C++ primer plus/前三章知识整合/a、第二章.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "This is my frist try to write a c++ programe!" << endl;
+	int My_salary{};
+	cin >> My_salary;
+	cout << "i'll get " << My_salary << " yuan per year!" << endl;
+
+
+
+
+	return 0;
+}
