e1b68ce49ade8f75d5f5c70b153e33259e44e821
pamapama88 <3512232084@qq.com>
2023-04-26 23:42:24 +0800

    稍做笔记

--- C++ primer plus/前三章知识整合/a、第二章.cpp
+++ C++ primer plus/前三章知识整合/a、第二章.cpp
@@ -9,6 +9,24 @@ int main()
 	cout << "i'll get " << My_salary << " yuan per year!" << endl;
 
 
+	//其实这是第四章的一个内容 
+
+		// 数组的 动态联编
+	int size;
+	cin >> size;
+	int* p = new int [size];
+	delete [] p;
+		//目前为止，我觉得这是一个完全正确且方便的动态联编过程（其实是照着课本抄的《圣经 P91》）
+
+		//My_test
+	char wenzi[20] = "zheshiyigezifuchuan";
+	cout<<"这是输出\"wenzi\"的结果：" << wenzi << endl;
+	char* p_wenzi = &wenzi[0];
+	cout << "这是输出\"wenzi[0]\"的地址p_wenzi的结果:" << p_wenzi << endl;
+		//结果他们是相同的，说明p_wenzi和wenzi都是将第一个元素输出之后，cout会接着向后索检直至遇见 \0 为止。
+
+
+
 
 
 	return 0;
