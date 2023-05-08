4b80873c837d732e2fd500856c47757669cee4c8
pamapama88 <3512232084@qq.com>
2023-05-08 23:59:18 +0800

    实验性文件

---
+++ 简单文件I O/文件I O.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_DEPRECATE 1
+#include<iostream>
+#include<fstream>
+#include<cstring>
+
+int main()
+{
+	char cccin{};
+	char chin[80]{ "abcdefgh" };
+	std::string  stin{ "abcdefgh" };
+	std::ofstream a_outfile;
+	std::ifstream a_infile;
+	a_outfile.open("test.txt");      //输出文字到文本文件中
+	a_infile.open("test.txt");       //从文本文件中获取文字
+	for (int i{}; i <= 57; i++)
+	{
+		a_outfile << "第" << i << "天的房价是：\n";
+	}
+	std::cin.getline(stin,90);
+	for (int i{}; i <= 57; i++)
+	{
+//		a_infile >> chin;
+		//a_infile >> stin;
+		a_infile >> cccin;
+		a_infile.getline(chin, 80);
+		std::cout << chin << std::endl;
+		std::cout << stin;
+		std::cout << cccin;
+	}
+
+
+	return 0;
+}
